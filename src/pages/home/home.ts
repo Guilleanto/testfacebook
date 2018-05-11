4c008ba355aefbe4e44d635aeb693a9a2deace75
Guillermo Ochoa <guillermoantonio8a@gmail.com>
2018-05-11 13:23:09 -0400

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+userdata = null;
+  constructor(private face: Facebook) {
 
   }
 
+  loginFB(){
+    this.face.login(['email','public_profile']).then((response: FacebookLoginResponse) => {
+     // response.authResponse.accessToken //FIREBASE TOKEN
+     this.face.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile =>{
+       this.userdata = {
+         email:      profile['email'],
+         first_name: profile['first_name'],
+         picture:    profile['picture_large']['data']['url'], 
+         username:   profile['name']};
+     })
+    })
+  }
 }
